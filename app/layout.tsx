72d0e1447e1de88b844df885d6549869987562ae
Tyler Sawchyn <tylerjsawchyn@gmail.com>
2026-08-22 10:31:53 -0600

    Apply neon arcade catalog theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
+import "./neon-override.css";
 
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000";
 
